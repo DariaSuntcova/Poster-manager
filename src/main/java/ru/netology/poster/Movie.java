306d87db595ddf2b3f26fb4059953af586f7a458
Suntcova Daria <batareyka23@gmail.com>
2022-08-09 19:24:18 +0300

    v1.2

--- src/main/java/ru/netology/poster/Movie.java
+++ src/main/java/ru/netology/poster/Movie.java
@@ -6,9 +6,27 @@ public class Movie {
     private String movieGenre;
 
 
+
+
     public Movie(String nameMovie, String movieGenre) {
         this.nameMovie = nameMovie;
         this.movieGenre = movieGenre;
 
+    }
+
+    public String getNameMovie() {
+        return nameMovie;
+    }
+
+    public void setNameMovie(String nameMovie) {
+        this.nameMovie = nameMovie;
+    }
+
+    public String getMovieGenre() {
+        return movieGenre;
+    }
+
+    public void setMovieGenre(String movieGenre) {
+        this.movieGenre = movieGenre;
     }
 }
